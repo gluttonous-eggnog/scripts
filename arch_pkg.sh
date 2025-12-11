ee9f71675e4c8376998964880bef0de0cf77ed82
gluttonous-eggnog <gluttonouseggnog@gmail.com>
2025-12-11 12:39:21 +1000

    Update arch_pkg.sh

--- arch_pkg.sh
+++ arch_pkg.sh
@@ -2,63 +2,32 @@
 
 FILE="$HOME/Documents/pkg.lst"
 BAK_FILE="${FILE}.bak"
+OUTPUT_FILE="$HOME/Documents/updated-pkg.txt"
 
-echo "** Update list of explicitly installed packages **"
-if [ -f "$FILE" ]; then
-    # Check if the backup exists
-    if [ -f "$BAK_FILE" ]; then
-
-        # Get current time and last modified time of the backup in seconds since epoch
-        CURRENT_TIME=$(date +%s)
-        BAK_MOD_TIME=$(stat -c %Y "$BAK_FILE")
-
-        # Calculate the difference in days
-        DIFF_DAYS=$(( (CURRENT_TIME - BAK_MOD_TIME) / 86400 ))
-
-        if [ "$DIFF_DAYS" -gt 2 ]; then
-            echo "Backup is older than 2 days. Overwriting..."
-            cp "$FILE" "$BAK_FILE"
-        else
-            echo "Backup is recent (less than or equal to 2 days). No action taken."
-        fi
-
-    else
-        echo "No backup file found. Creating backup..."
-        cp "$FILE" "$BAK_FILE"
-    fi
-
-    # Now update the package list
-    echo "Updating '$FILE' now.."
-    pacman -Qe > "$FILE"
-    echo "Done."
-
-else
-    echo "File '$FILE' does not exist. Creating now..."
-    pacman -Qe > "$FILE"
-fi
-
-# Now update recently updated packages for potential partial upgrade fails
 # Get the most recent Monday (today if it's Monday)
 monday=$(date -d "last monday" +"%Y-%m-%d")
 if [[ $(date +%u) -eq 1 ]]; then
-    monday=$(date +"%Y-%m-%d")
+    monday="$(date +"%Y-%m-%d")"
 fi
-currentdate=$(date +"%Y-%m-%d")
+currentdate="$(date +"%Y-%m-%d")"
 
-echo "*****************************************************"
-echo "*** GET A LIST OF UPGRADED PKGS SINCE: $monday ***"
-echo "*****************************************************"
-
-OUTPUT_FILE="$HOME/Documents/updated-pkg.txt"
-
+echo "-------------------------------------------------------------"
+echo "******* GET A LIST OF UPGRADED PKGS SINCE: $monday *******"
+echo "-------------------------------------------------------------"
 echo "Packages upgraded since: $monday" > "$OUTPUT_FILE"
 echo >> "$OUTPUT_FILE"
 
 # Convert start and end dates to timestamps
-STARTD=$(date -d "$monday" +%s)
-ENDD=$(date -d "$currentdate +1 day" +%s)
+STARTD="$monday"
+ENDD="$(date -d "$currentdate +1 day" +'%Y-%m-%d')"
 
 # Query packages installed and save to file
 echo "Querying packages installed since $monday"
-expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | awk -v start="$monday" -v end="$currentdate" '($1 >= start && $1 <= end)' >> "$OUTPUT_FILE"
+expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | awk -v start="$STARTD" -v end="$ENDD" '$1 >= start && $1 <= end' > "$OUTPUT_FILE"
 echo "Upgraded list saved to $OUTPUT_FILE"
+echo "-------------------------------------------------------------"
+echo "Update list of explicitly installed packages..."
+echo "Querying using pacman -Qe since $monday"
+rg -F -f <(pacman -Qe) "$OUTPUT_FILE" > "$FILE"
+echo "Explicitly installed list save to $FILE"
+echo "-------------------------------------------------------------"
